d0a1ae90bfb309b56864573b5909e55dd501c2f4
Ralielly Silva <silvaralielly0@gmail.com>
2024-08-22 11:40:59 -0300

    aula-22-08

---
+++ atv-22-08/imc2.js
@@ -0,0 +1,11 @@
+ // Entreda
+        
+ let peso =Number(prompt("Digite seu peso"))
+ let altura = Number(prompt("Digite sua altura"))
+
+ // Processamento
+ let imc = peso / (altura * altura)
+
+ // Saida
+ document.write("IMC =",imc)
+
