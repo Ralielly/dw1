6ad2ee86a3b7f0d32c34e404d0a502bd50a98168
Ralielly Silva <silvaralielly0@gmail.com>
2024-10-24 10:24:04 -0300

    .

---
+++ ex01-ex02/ex01.js
@@ -0,0 +1,50 @@
+function somaSerie(count) {
+    let = 0
+
+    for(let x=1; x<=count; x++){
+        soma = soma + (x * y)
+        y += 2
+
+}
+    return soma
+
+}
+
+// console.log(somaSerie(3))
+// console.log(somaSerie(4))
+
+
+
+// count = 4 
+
+// let soma = 0
+// let y = 1
+// for(let x=1; x<=count; x++){
+//     soma = soma + (x * y)
+//     y += 2
+
+// }
+// console.log(soma)
+
+
+
+
+
+//                x   y
+// soma = soma + (1 * 1)
+// soma = soma + (2 * 3)
+// soma = soma + (3 * 5)
+// soma = soma + (4 * 7)
+
+
+
+
+
+
+
+            //   x   x+1
+// soma = soma + (1 * 1)
+// soma = soma + (2 * 3)
+// soma = soma + (3 * 5)
+// soma = soma + (4 * 7)
+
