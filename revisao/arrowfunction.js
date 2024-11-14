641fa1370b63e18fbbc0c2270fda9632e5ae79cf
Ralielly Silva <silvaralielly0@gmail.com>
2024-11-14 11:51:58 -0300

    dia14-11

---
+++ revisao/arrowfunction.js
@@ -0,0 +1,13 @@
+const saudacaoPadrao = (nome) => console.log(`Ola ${nome}` )
+const saudacao = nome => console.log(`Ola ${nome}`)
+
+const soma = (a,b) => Math.pow(a,b)
+
+
+
+saudacaoPadrao()
+saudacao("João")
+console.log(soma(2,3))
+
+
+
---
+++ revisao/revisao.js
@@ -0,0 +1,16 @@
+const numeros = [10, 15 , 20]
+
+
+
+
+function multiplica2(elemento) {
+    return elemento
+}
+
+const copia = numeros.map(multiplica2)
+
+console.log(copia)
+
+
+
+
